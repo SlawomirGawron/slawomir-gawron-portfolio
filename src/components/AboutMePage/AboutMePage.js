a0ef2ef283bb5c5793d3effa0a03daee9aeee671
Slawomir Gawron <slawomirgawron8@gmail.com>
2019-06-21 16:53:30 -0400

    Scrollbar for about me page plus flex addition done.

--- src/components/AboutMePage/AboutMePage.js
+++ src/components/AboutMePage/AboutMePage.js
@@ -4,55 +4,9 @@ import { Scrollbars } from 'react-custom-scrollbars';
 import './AboutMePage.scss';
 
 class AboutMePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            windowWidth: 0,
-            windowHeight: 0
-        };
-
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
-
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-
-    componentDidUpdate() {
-        const { windowWidth, windowHeight} = this.state;
-        const currentWidth=window.innerWidth;
-        const currentHeight=window.innerHeight;
-
-        if ( (currentWidth !== windowWidth)
-             || (currentHeight !== windowHeight) ) {
-            this.updateWindowDimensions();
-        }
-    }
-
-    updateWindowDimensions() {
-        this.setState({
-            windowWidth: window.innerWidth,
-            windowHeight: window.innerHeight
-        });
-    }
-
     render() {
-        const { windowWidth, windowHeight} = this.state;
-        console.log("width: " + this.state.width );
-        console.log("height: " + this.state.height);
-
         return (
-            <div className="about-me-page"
-                 style={{
-                     width: windowWidth,
-                     height:(windowHeight*0.9)
-                 }}
-            >
+            <div className="about-me-page">
                 <Scrollbars
                     autoHide={false}
                     renderTrackVertical={props => <div {...props} className="track-vertical"/>}
