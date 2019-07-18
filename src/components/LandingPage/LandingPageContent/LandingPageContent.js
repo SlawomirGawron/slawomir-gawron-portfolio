976eefab2018fc77771d6bad0492041550a67213
Slawomir Gawron <slawomirgawron8@gmail.com>
2019-07-18 19:16:36 -0400

    Checking branch deletion.

--- src/components/LandingPage/LandingPageContent/LandingPageContent.js
+++ src/components/LandingPage/LandingPageContent/LandingPageContent.js
@@ -1,47 +1,34 @@
 import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
-import avatar from 'src/images/Slawomir-Gawron-Photo.png';
-
 import './LandingPageContent.scss';
+import {Link} from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 class LandingPageContent extends Component {
     render() {
         return (
             <div className="landing-page-content">
-                <Grid container
-                      spacing={16}
-                      className="grid-container"
-                      alignItems="center"
-                      direction="column"
-                      justify="center"
-                >
-                    <Grid item xs={12} sm={12} md={12} lg={12}>
-                        <img
-                            src={avatar}
-                            alt="portfolio-avatar"
-                            className="portfolio-photo"
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={12} md={12} lg={12}>
-                        <div className="banner-text">
-                            <h1>Junior Front End Web Developer</h1>
-                            <hr />
-                            <p> React | HTML/CSS | JavaScript | Java | SQL </p>
-                            <hr />
-                            <div className="social-links">
-                                {/* Linkedin */}
-                                <a href="https://www.linkedin.com/in/slawomir-gawron/" rel="noopener noreferrer" target="_blank">
-                                    <i className="fab fa-linkedin"/>
-                                </a>
+                <div className="banner-text">
+                    <h1>Slawomir Gawron</h1>
+                    <h1>Junior Front End Web Developer</h1>
+                    <p> React | HTML/CSS | JavaScript | Java | SQL </p>
+                    <div className="social-links">
+                        {/* Linkedin */}
+                        <a href="https://www.linkedin.com/in/slawomir-gawron/" rel="noopener noreferrer" target="_blank">
+                            <i className="fab fa-linkedin"/>
+                        </a>
 
-                                {/* Github */}
-                                <a href="https://github.com/SlawomirGawron" rel="noopener noreferrer" target="_blank">
-                                    <i className="fab fa-github"/>
-                                </a>
-                            </div>
-                        </div>
-                    </Grid>
-                </Grid>
+                        {/* Github */}
+                        <a href="https://github.com/SlawomirGawron" rel="noopener noreferrer" target="_blank">
+                            <i className="fab fa-github"/>
+                        </a>
+                    </div>
+                    <Button component={Link} to="/resume" variant="contained">
+                        <Typography variant="button">
+                            About Me
+                        </Typography>
+                    </Button>
+                </div>
             </div>
         );
     }
