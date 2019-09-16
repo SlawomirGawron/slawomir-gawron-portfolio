b333dbef50882a7cdbd79b6e42f017c8bdfc0a22
Slawomir Gawron <slawomirgawron8@gmail.com>
2019-09-16 18:38:21 -0400

    Added shopping cart to projects.

--- src/components/ProjectsPage/ProjectsPage.js
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -4,6 +4,7 @@ import Tab from '@material-ui/core/Tab';
 import Grid from '@material-ui/core/Grid';
 import ProjectCard from "src/components/ProjectsPage/projectCard/ProjectCard";
 import TicTacToeGameImage from "src/images/TicTacToe.png";
+import ShoppingCartImage from "src/images/shopping-cart-temp.jpg";
 import addMenuBarToComponent from "src/HOC/addMenuBarToComponent/addMenuBarToComponent";
 
 import './ProjectsPage.scss';
@@ -23,75 +24,98 @@ class ProjectsPage extends Component {
     toggleCategories() {
         const { activeTab } = this.state;
 
-        // React
         if (activeTab === 0) {
             return (
-                <ProjectCard
-                    component="img"
-                    alt="project-0"
-                    maxHeight= "500px"
-                    maxWidth= "500px"
-                    imageHeight={250}
-                    imageWidth={400}
-                    image={TicTacToeGameImage}
-                    title="Tic Tac Toe"
-                    summary="A simple Tic Tac Toe game built using react."
-                    onImageClickURL="https://sgawron-react-game.netlify.com/"
-                    buttonsList={
-                        [
-                            {text: "Learn More", url: "https://en.wikipedia.org/wiki/Tic-tac-toe"},
-                            {text: "Game Link", url: "https://sgawron-react-game.netlify.com/"},
-                            {text: "Github", url: "https://github.com/SlawomirGawron/react-game"},
-                        ]
-                    }
-                />
+                <div className="react-projects">
+                    <ProjectCard
+                        component="img"
+                        alt="project-0"
+                        maxHeight= "500px"
+                        maxWidth= "500px"
+                        imageHeight={250}
+                        imageWidth={400}
+                        image={TicTacToeGameImage}
+                        title="Tic Tac Toe"
+                        summary="A simple Tic Tac Toe game built using react."
+                        onImageClickURL="https://sgawron-react-game.netlify.com/"
+                        buttonsList={
+                            [
+                                {text: "Learn More", url: "https://en.wikipedia.org/wiki/Tic-tac-toe"},
+                                {text: "Game Link", url: "https://sgawron-react-game.netlify.com/"},
+                                {text: "Github", url: "https://github.com/SlawomirGawron/react-game"},
+                            ]
+                        }
+                    />
+                    <ProjectCard
+                        component="img"
+                        alt="project-1"
+                        maxHeight= "500px"
+                        maxWidth= "500px"
+                        imageHeight={250}
+                        imageWidth={400}
+                        image={ShoppingCartImage}
+                        title="Shopping Cart"
+                        summary="A shopping cart like app built using React Hooks, Redux, and Canadian Sales Tax API."
+                        onImageClickURL="https://sgawron-shopping-cart.netlify.com/"
+                        buttonsList={
+                            [
+                                {text: "Learn More", url: "https://en.wikipedia.org/wiki/Shopping_cart_software"},
+                                {text: "Site Link", url: "https://sgawron-shopping-cart.netlify.com"},
+                                {text: "Github", url: "https://github.com/SlawomirGawron/shopping-cart"},
+                            ]
+                        }
+                    />
+                </div>
+
             );
-        // Java
         } else if (activeTab === 1) {
             return (
-                <ProjectCard
-                    component="img"
-                    alt="project-1"
-                    maxHeight= "500px"
-                    maxWidth= "500px"
-                    imageHeight={250}
-                    imageWidth={400}
-                    image="https://assets.exercism.io/tracks/java-bordered-turquoise.png"
-                    title="Title 1"
-                    summary="Tab 1 summary"
-                    onImageClickURL="https://react-redux.js.org/"
-                    buttonsList={
-                        [
-                            {text: "Learn More", url: "https://www.google.ca/"},
-                            {text: "button 1", url: "https://react-redux.js.org/"},
-                            {text: "button 2", url: "https://react-redux.js.org/"},
-                        ]
-                    }
+                <div className="java-projects">
+                    <ProjectCard
+                        component="img"
+                        alt="project-10"
+                        maxHeight= "500px"
+                        maxWidth= "500px"
+                        imageHeight={250}
+                        imageWidth={400}
+                        image="https://assets.exercism.io/tracks/java-bordered-turquoise.png"
+                        title="Title 1"
+                        summary="Tab 1 summary"
+                        onImageClickURL="https://react-redux.js.org/"
+                        buttonsList={
+                            [
+                                {text: "Learn More", url: "https://www.google.ca/"},
+                                {text: "button 1", url: "https://react-redux.js.org/"},
+                                {text: "button 2", url: "https://react-redux.js.org/"},
+                            ]
+                        }
 
-                />
+                    />
+                </div>
             );
-        // Other
         } else if (activeTab === 2) {
             return (
-                <ProjectCard
-                    component="img"
-                    alt="project-2"
-                    maxHeight= "500px"
-                    maxWidth= "500px"
-                    imageHeight={250}
-                    imageWidth={400}
-                    image="https://raddevon.com/wp-content/uploads/2018/10/react.jpg"
-                    title="Title 2"
-                    summary="Tab 2 summary"
-                    onImageClickURL="https://react-redux.js.org/"
-                    buttonsList={
-                        [
-                            {text: "Learn More", url: "https://www.google.ca/"},
-                            {text: "button 1", url: "https://react-redux.js.org/"},
-                            {text: "button 2", url: "https://react-redux.js.org/"},
-                        ]
-                    }
-                />
+                <div className="other-projects">
+                    <ProjectCard
+                        component="img"
+                        alt="project-2"
+                        maxHeight= "500px"
+                        maxWidth= "500px"
+                        imageHeight={250}
+                        imageWidth={400}
+                        image="https://raddevon.com/wp-content/uploads/2018/10/react.jpg"
+                        title="Title 2"
+                        summary="Tab 2 summary"
+                        onImageClickURL="https://react-redux.js.org/"
+                        buttonsList={
+                            [
+                                {text: "Learn More", url: "https://www.google.ca/"},
+                                {text: "button 1", url: "https://react-redux.js.org/"},
+                                {text: "button 2", url: "https://react-redux.js.org/"},
+                            ]
+                        }
+                    />
+                </div>
             );
         } else {
             return (
